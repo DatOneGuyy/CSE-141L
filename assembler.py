fc23d88fcadacc8d9ef254de4bf63877bfd653ea
Daniel <computingpotatochips@gmail.com>
2026-04-06 20:15:47 -0700

    finished assembler and compiled

--- assembler.py
+++ assembler.py
@@ -3,6 +3,7 @@ import time
 
 parser = argparse.ArgumentParser(description="Assembler")
 parser.add_argument("name", help="File to read")
+parser.add_argument("-o", "--output", help="Output file to write binary to", default="out")
 
 args = parser.parse_args()
 
@@ -57,15 +58,15 @@ delabeled_data = []
 for line in data:
     if line[0] == "#":
         if line in mapping.keys():
-            print(f"Duplicate label definintion detected at pc value {pc}. Exiting")
+            print(f"[ERROR] Duplicate label definintion detected at pc value {pc}.")
             exit()
-        print(f"Identified label {line} at pc value {pc}")
+        #print(f"Identified label {line} at pc value {pc}")
         mapping[line] = (label_count, pc)
         label_count += 1
     else:
         pc += 1
         delabeled_data.append(line)
-print(f"Identified {label_count} labels")
+print(f"[INFO] Identified {label_count} labels.")
 label_memory = ["000000000000"] * 64
 
 # detect incorrectly used labels
@@ -78,23 +79,23 @@ for entry in mapping:
             # print(f"first split: {line.split(" ")}")
             token = line.split(" ")[0]
             if token == "jump":
-                print(f"Identified label {entry} as unconditional")
+                #print(f"Identified label {entry} as unconditional")
                 break
             elif token == "jumplt":
-                print(f"Identified label {entry} as conditional")
+                #print(f"Identified label {entry} as conditional")
                 break
             elif token == "call":
-                print(f"Identified label {entry} as a function")
+                #print(f"Identified label {entry} as a function")
                 break
 
     for line in marked_data:
         if (entry + "`") in line:
             current_token = line.split(" ")[0]
             if current_token != token and current_token[:-1] != entry:
-                print(f"Invalid use of label {entry}. First use: {token}, found another use for {current_token}. Labels can only be used with one jump type.")
+                print(f"[ERROR] Invalid use of label {entry}. First use: {token}, found another use for {current_token}. Labels can only be used with one jump type.")
                 exit()
     if token == "none":
-        print(f"Label {entry} is never jumped to. Defaulting to unconditional.")
+        print(f"[WARNING] Label {entry} is never jumped to. Defaulting to unconditional.")
         token = "jump"
     
     label_memory[mapping[entry][0]] = bin(mapping[entry][1])[2:]
@@ -109,11 +110,12 @@ for entry in mapping:
             label_memory[mapping[entry][0]] += "10"
 #for entry in label_memory:
     #print(entry)
-print("Filled label memory.")
+print("[INFO] Filled label memory.")
 
 instructions = []
 #fill in instructions
 for line in delabeled_data:
+    #print(f"Assembling line {line}")
     tokens = line.split(" ")
     code = opcodes[tokens[0]]
     match tokens[0]:
@@ -124,14 +126,82 @@ for line in delabeled_data:
         case "jumplt":
             code += pad_zeroes(bin(mapping[tokens[1]][0])[2:], 6)
         case "cmp":
-            code += pad_zeroes(bin(tokens[1][1])[2:], 3)
-            code += pad_zeroes(bin(tokens[2][1])[2:], 3)
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += pad_zeroes(bin(int(tokens[2][1]))[2:], 3)
+        case "copy":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            if len(tokens) == 3:
+                code += pad_zeroes(bin(int(tokens[2][1]))[2:], 3)
+            else:
+                code += "000"
+        case "push":
+            code += "0"
+            code += pad_zeroes(bin(int(tokens[1][1]) - 1)[2:], 2)
+            code += "000"
+        case "pop":
+            code += "100000"
+        case "return":
+            code += "111000"
+        case "imm":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            if tokens[2] == "-1":
+                code += "111"
+            else:
+                code += pad_zeroes(bin(int(tokens[2]))[2:], 3)
+        case "load":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "1"
+            if len(tokens) == 3:
+                code += pad_zeroes(bin(int(tokens[2]))[2:], 2)
+            else:
+                code += "00"
+        case "store":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "0"
+            if len(tokens) == 3:
+                code += pad_zeroes(bin(int(tokens[2]))[2:], 2)
+            else:
+                code += "00"
+        case "add":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += pad_zeroes(bin(int(tokens[2][1]))[2:], 3)
+        case "lshift":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "0"
+            if tokens[2] == "4":
+                code += "00"
+            else:
+                code += pad_zeroes(bin(int(tokens[2]))[2:], 2)
+        case "rshift":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "1"
+            if tokens[2] == "4":
+                code += "00"
+            else:
+                code += pad_zeroes(bin(int(tokens[2]))[2:], 2)
+        case "dist":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "011"
+        case "not":
+            code += pad_zeroes(bin(int(tokens[1][1]))[2:], 3)
+            code += "101"
+        case _:
+            print(f"[ERROR] Invalid instruction at {line}.")
     
     instructions.append(code)
+    if len(code) != 9:
+        print(f"[ERROR] Instruction length error on line {line}.")
+        print(f"[ERROR] Instruction: {code}.")
+    #print(code)
 
-for line in instructions:
-    print(line)
+print("[INFO] Filled instruction memory.")
 
 end = time.perf_counter()
-print(f"Assembling time: {(end - start):.6f} seconds")
+print(f"[INFO] Assembling time: {(end - start) * 1000:.3f} ms")
+
+file.close()
+
+file = open(args.output + ".bin", "w")
+file.writelines(instructions)
+print(f"[INFO] Saved machine code to {args.output}.bin")
 file.close()
---
+++ multiply.asm
@@ -0,0 +1,6 @@
+//main function 3
+#program-3-multiply
+
+    jump #end
+
+#end
