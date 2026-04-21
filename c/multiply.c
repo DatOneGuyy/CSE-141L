d95a84b14b0c24760042b44baec1be1407b48068
DatOneGuyy <computingpotatochips@gmail.com>
2026-04-20 18:20:41 -0700

    update multiply.c

--- c/multiply.c
+++ c/multiply.c
@@ -14,13 +14,28 @@ void initialize() {
     }
 }
 
+int32_t tester(uint8_t msb1, uint8_t lsb1, uint8_t msb2, uint8_t lsb2) {
+    int16_t multiplier = ((int16_t)msb1 << 8) | (int16_t)lsb1;
+    int16_t multiplicand = ((int16_t)msb2 << 8) | (int16_t)lsb2;
+
+    int32_t product = (int32_t)multiplier * (int32_t)multiplicand;
+
+    return product;
+}
+
+int32_t read_int(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
+    int32_t result = ((int32_t)a << 24) | ((int32_t)b << 16) | ((int32_t)c << 8) | (int32_t)d;
+
+    return result;
+}
+
 //register file flag
 int is_positive(uint8_t num) {
     return (num < 128);
 }
 
 void multiply() {
-    uint32_t product;
+    uint32_t product = 0;
 
     uint8_t counter = 16;
 
@@ -53,11 +68,35 @@ void multiply() {
 
 int main() {
     initialize();
+    
+    printf("Raw: ");
+    for (int i = 0; i < 64; i++) {
+        uint8_t value = zero_address[i];
+        printf("%u ", value);
+    }
+    printf("\n");
+
+    printf("Input: ");
+    for (int i = 0; i < 64; i += 2) {
+        int16_t value = ((int16_t)zero_address[i] << 8) | (int16_t)zero_address[i + 1];
+
+        if (value > 32767) value -= 65536;
+        printf("%d ", value);
+    }
+    printf("\n");
+
+    printf("Expected: ");
+    for (int i = 0; i < 64; i += 4) {
+        int32_t expected = tester(zero_address[i], zero_address[i + 1], zero_address[i + 2], zero_address[i + 3]);
+
+        printf("%d ", expected);
+    }
+    printf("\n");
 
     uint8_t max_count = 64;
     uint8_t write_address = max_count;
 
-    for (uint8_t i = 0; max_count < 64; i += 4) {
+    for (uint8_t i = 0; i < max_count; i += 4) {
         uint8_t lsb1, lsb2;
         uint8_t msb1 = zero_address[i];
         uint8_t msb2 = zero_address[i + 2];
@@ -100,7 +139,7 @@ int main() {
 
         multiply();
         
-        int32_t product = (zero_address[3] << 24) | (zero_address[2] << 16) | (zero_address[1] << 8) | zero_address[0];
+        int32_t product = ((uint32_t)zero_address[3] << 24) | ((uint32_t)zero_address[2] << 16) | ((uint32_t)zero_address[1] << 8) | (uint32_t)zero_address[0];
         if (result_sign) {
             product *= -1;
         }
@@ -111,7 +150,14 @@ int main() {
         zero_address[write_address + 3] = product & 0xFF;
 
         write_address += 4;
+    }    
+
+    printf("Actual: ");
+    for (int i = 64; i < 128; i += 4) {
+        int32_t actual = read_int(zero_address[i], zero_address[i + 1], zero_address[i + 2], zero_address[i + 3]);
+        printf("%d ", actual);
     }
+    printf("\n");
 
     free(zero_address);
 
