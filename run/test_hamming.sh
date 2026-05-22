4edfec2e5c352c1499f4ff7a651c0381d47d731d
DatOneGuyy <computingpotatochips@gmail.com>
2026-05-22 15:49:11 -0700

    started program 1 testbenching

---
+++ run/test_hamming.sh
@@ -0,0 +1,12 @@
+rm obj_dir/test_hamming
+python3 ../assembly/assembler.py ../assembly/hamming.asm -b -o hamming
+mv hamming.bin ../rtl/programs/hamming.bin
+verilator --binary -j 0 -o test_hamming --trace ../rtl/packages/types.sv ../rtl/*.sv ../tb/hamming_tb.sv
+rm obj_dir/*.h
+rm obj_dir/*.cpp
+rm obj_dir/*.a
+rm obj_dir/*.d
+rm obj_dir/*.o
+rm obj_dir/*.dat
+rm obj_dir/*.mk
+./obj_dir/test_hamming
---
+++ run/test_hamming_custom.sh
@@ -0,0 +1,12 @@
+rm obj_dir/test_hamming_custom
+python3 ../assembly/assembler.py ../assembly/hamming.asm -b -o hamming
+mv hamming.bin ../rtl/programs/hamming.bin
+verilator --binary -j 0 -o test_hamming_custom --trace ../rtl/packages/types.sv ../rtl/*.sv ../tb/hamming_tb_custom.sv
+rm obj_dir/*.h
+rm obj_dir/*.cpp
+rm obj_dir/*.a
+rm obj_dir/*.d
+rm obj_dir/*.o
+rm obj_dir/*.dat
+rm obj_dir/*.mk
+./obj_dir/test_hamming_custom
